99dfd627550e674a4ac49a6c454c0a94bb1c5ed6
cgutierrez7 <chris.n.gutierrez@gmail.com>
2021-06-07 13:10:11 -0700

    store with methods and state added

--- implementing-redux/counter.js
+++ implementing-redux/counter.js
@@ -1,2 +1,26 @@
 import Dedux from './dedux.js'
 const { createStore } = Dedux
+
+
+export const increaseAction = () => {
+    return { type: "increase" }
+}
+  
+export const decreaseAction = () => {
+    return { type: "decrease" }
+}
+  
+const reducer = (state, action = {}) => {
+  switch (action.type) {
+    case "increase":
+      return { ...state, count: state.count + 1, };
+    case "decrease":
+      return { ...state, count: state.count - 1, };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer)
+
+export default store
